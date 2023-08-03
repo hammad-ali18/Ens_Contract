3b933c3327ddb5cac3723c254ef6f2fd25a2f683
hammad-ali18 <hammadali0298@gmail.com>
2023-08-03 17:14:39 +0500

    fifth commit

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const { BigNumber } = require('bignumber.js')
 const { expect } = require("chai");
 const { reverse } = require("dns");
-const { poll } = require("ethers/lib/utils");
+const { poll} = require("ethers/lib/utils");
 const { waffle,ethers } = require("hardhat");
 const { userInfo } = require("os");
 const { register } = require("ts-node");
@@ -29,12 +29,12 @@ const maxCommitmentAge=3600
 
 const name  = 'hammad.eth';
 
-describe('Greeter', () =>{
+describe('Testing ENS Contracts', () =>{
 
     const [owner, accountOne] = provider.getWallets();
 
 
-    beforeEach( async () =>{
+    before( async () =>{
         Greeter = await ethers.getContractFactory("Greeter");
         greeter = await Greeter.deploy("Hello World");
         
@@ -85,85 +85,86 @@ describe('Greeter', () =>{
         
         namewrap = await namewrapper.deploy(ens.address,basereg.address,metadata.address)
         console.log("NameWrapper test: ",namewrap.address)
-        let resolver =  await ens.resolver(baseNode)
+     resolver =  await ens.resolver(baseNode)
         console.log("resolver address: ",resolver)
 
         
-        const hash  = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(name));
-        console.log("hash of ens name: ",hash)
- 
-        subname = "subMuhammad"
-        const label =  ethers.utils.keccak256(ethers.utils.toUtf8Bytes(subname));
-        //can find this function on Activity of MetaMask, this setSubnodeRecord() is on NameWrapper
-        console.log("Subdomain name: ", label)
-        setsubnoderecord = await   namewrap.setSubnodeRecord(hash,label,owner.address,resolver,31536000)
-        // console.log(setsubnoderecord)
+        
+        
+        
+        
+        
     })
-
-
-it('Should return set string', async () =>{
     
-    
-    
+    it('Register a Domain', async () =>{
+        
+        
         let commitment = await ethregcontroller.makeCommitmentWithConfig(name,owner.address,"0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef","0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000")
         console.log("Commitment: ",commitment)
         await ethregcontroller.commit(commitment);
         const duration = 604800*5
         console.log("82")
-    //     await waffle.provider.send("evm_setNextBlockTimestamp", [Math.floor(Date.now() / 1000) + 60]);
-    //     // Mine a new block with the updated timestamp
+        //     await waffle.provider.send("evm_setNextBlockTimestamp", [Math.floor(Date.now() / 1000) + 60]);
+        //     // Mine a new block with the updated timestamp
     // await waffle.provider.send("evm_mine");
           await provider.send('evm_increaseTime', [61]); // Increase by 60 seconds
-    await provider.send('evm_mine'); // Mine a new block to update the timestamp
-console.log("86")
+          await provider.send('evm_mine'); // Mine a new block to update the timestamp
+          console.log("86")
 
-
-let setrecords = await ens.setRecord(baseNode,owner.address,resolver,31536000)
-
-
+          
+          let setrecords = await ens.setRecord(baseNode,owner.address,resolver,31536000)
+          
+          
 let addcontroller = await basereg.addController(ethregcontroller.address)
-// console.log(addcontroller.address)
-// await basereg.removeController(basereg.address)
+
+
 console.log("96")
 let setowner = await ens.setOwner(baseNode,basereg.address)
 
 let owneraddress = await ens.owner(baseNode)
-console.log("owner address: ",owneraddress)
+console.log("owner address: ",owner.address)
 
- let rentprice =  await ethregcontroller.rentPrice(name,duration);
- console.log(rentprice)
-        let regConfig = await ethregcontroller.register(name,owner.address,duration,"0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",{value:rentprice});
-     
-        // console.log("regConfig:",regConfig)
-        // await ethregcontroller.register(reg)
-       console.log(" line 83")
+let rentprice =  await ethregcontroller.rentPrice(name,duration);
+console.log("rentprice: ", rentprice)
+let register = await ethregcontroller.register(name,owner.address,duration,"0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",{value:rentprice});
 
-        // let myEns="0x9adac4ff802de6451124066ebbf37ea63a52852fe7f05f543075f60347fabfae"
-        //    let addressBigNumber = new BigNumber(myEns.slice(2),16)
-       
-        let id = ethers.BigNumber.from(hash);
-        let id1 =  await basereg.nameExpires(id)
-              console.log(id1);
 
-              let commitTime = await ethregcontroller.commitments("0x83ddcb8c766de449d2bdd8f7e0649e037d60bf29e793ea44c66d6f5efa5be960");
-              console.log("commit Time",commitTime)      
+// let myEns="0x9adac4ff802de6451124066ebbf37ea63a52852fe7f05f543075f60347fabfae"
+//    let addressBigNumber = new BigNumber(myEns.slice(2),16)
+
+//  id = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(ethers.BigNumber.from(hash)));
+
+console.log(" line 139")
+let commitTime = await ethregcontroller.commitments("0x83ddcb8c766de449d2bdd8f7e0649e037d60bf29e793ea44c66d6f5efa5be960");
+console.log("commit Time",commitTime)      
 
 //               let register = await ethregcontroller.register(name,owner.address,duration,"0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef");
 // console.log(register)
 
 
-              //   await exp.price("hammad.eth")const { BigNumber } = require('bignumber.js')
+})
 
-//       let reg = await ethregcontroller.register("hammad.eth",owner.address,duration,"0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef");
-//       console.log(reg);
-  
+it('Register a Sub Domain', async () =>{
+   let hash  = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(name));
+    console.log("hash of ens name: ",hash)
 
+    let id =ethers.utils.defaultAbiCoder.encode(['uint256'],[ethers.BigNumber.from(hash)]);
+     
+        
+        console.log("id: ",id)
+        
+        subname = "subMuhammad"
+        label =  ethers.utils.keccak256(ethers.utils.toUtf8Bytes(subname));
+        //can find this function on Activity of MetaMask, this setSubnodeRecord() is on NameWrapper
+        console.log("Subdomain name: ", label)
+        // setsubnoderecord = await   namewrap.setSubnodeRecord(hash,label,owner.address,resolver,31536000)
+        // console.log(setsubnoderecord)
 
+        subnode = ethers.utils.solidityKeccak256(['bytes32','bytes32'],[baseNode, id]);
+        console.log("testsubnode: ",subnode)
+        console.log("103")
 
-
-      
-
-      
+setsubnoderecord = await ens.setSubnodeRecord(subnode,label,owner.address,resolver,31536000)
     })
 
    
